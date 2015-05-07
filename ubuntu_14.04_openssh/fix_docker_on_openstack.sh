e11b54abc28a54fa37e71bf3fe82c3be3f883d0d
Alin CALINCIUC <alin.calinciuc@gmail.com>
2015-05-07 19:31:43 +0300

    initial push

---
+++ ubuntu_14.04_openssh/fix_docker_on_openstack.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sleep 5
+
+# Get public keys from meta-data server
+# Create .ssh directory for root
+if [ ! -d /root/.ssh ]; then
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+fi
+# Fetch public key 
+ATTEMPTS=30
+FAILED=0
+if [ ! -f /root/.ssh/authorized_keys ]; then
+    wget http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key -O /tmp/metadata-key -o /var/log/metadata_svc_bugfix.log
+    if [ $? -eq 0 ]; then
+        cat /tmp/metadata-key >> /root/.ssh/authorized_keys
+        chmod 0600 /root/.ssh/authorized_keys
+        # restorecon /root/.ssh/authorized_keys
+        rm -f /tmp/metadata-key
+        echo "Successfully retrieved public key from instance metadata" >> /var/log/metadata_svc_bugfix.log
+    fi
+fi
+sync
+
+# Add instance name to /tmp/hosts
+instance_name=$(curl http://169.254.169.254/latest/meta-data/hostname)
+sed -i " 1 s/.*/& $instance_name/" /etc/hosts
+
+
+
