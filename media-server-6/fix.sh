dc98f3aaf37b0967488cb28f20f7d11dc96fdc62
Alin CALINCIUC <alin.calinciuc@gmail.com>
2016-01-28 21:47:29 +0200

    fixed sendstats and collectd hsotname

--- media-server-6/fix.sh
+++ media-server-6/fix.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+sleep 5
+
+instance_name=$(curl http://169.254.169.254/latest/meta-data/hostname)
+HOSTNAME_MONITORING="HOSTNAMEMONITORING"
+instance_name=${instance_name::-10}
+sed -i -e "s/$HOSTNAME_MONITORING/$instance_name/g" /etc/collectd/collectd.conf
+service collectd restart
+
+
 # Remove ipv6 local loop until ipv6 is supported
 instance_name_nova=$(curl http://169.254.169.254/latest/meta-data/hostname)
-IFS='.' read -ra array <<< "$instance_name_nova"
-instance_name=${array[0]}
 instance_name_local=$("hostname")
 echo "" > /etc/hosts
 echo "127.0.0.1 localhost $instance_name $instance_name_local" > /etc/hosts
@@ -11,7 +18,3 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 # Restart kurento
 service kurento-media-server-6.0 restart
-
-HOSTNAME_MONITORING="HOSTNAMEMONITORING"
-sed -i -e "s/$HOSTNAME_MONITORING/$instance_name/g" /etc/collectd/collectd.conf
-service collectd restart
